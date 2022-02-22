281dcecb9a48bf0f0bcb6015cf5159e727f19d96
Daniel Folomkin Boldyrev <daniel773@icloud.com>
2022-02-22 17:37:51 +0100

    ATM modification
    code is more complicated.

--- atm.py
+++ atm.py
@@ -3,50 +3,47 @@ passwordDB = "123"
 balance = 1000
 
 
+def execute():
+    option = int(input(
+        """\nHow do you want to proceed?
+    1. Deposite
+    2. Withdraw
+    3. Exit
+    your option --> """))
+    global balance
+    ok, balance = action(option)
+    if ok:
+        print("Action successfully done...")
+        print("New balance:", balance)
+    else:
+        print("Action not permitted...")
+        print("Balance:", balance)
+
+
 def login():
     print("Welcome")
     user = input("\tuser: ")
     password = input("\tpassword: ")
-    return check(user, password)
-
-
-def check(user, password):
     return user == userDB and password == passwordDB
 
 
 def action(option):
     if option == 1:
         amount = int(input("Enter amount to deposit: "))
-        return deposite(amount)
+        return True, balance + amount
     elif option == 2:
         amount = int(input("Enter amount to withdraw: "))
-        return withdraw(amount)
+        if amount > balance:
+            return False, balance
+        return True, balance - amount
+    elif option == 3:
+        quit('Goodbye!!!')
     return False, balance
 
 
-def deposite(amount):
-    return True, balance + amount
+if login():
+    while True:
+        execute()
+else:
+    print("Sorry, user and/or password are incorrect...")
 
-
-def withdraw(amount):
-    if amount > balance:
-        return False, balance
-    return True, balance - amount
-
-
-def execute():
-    if not login():
-        print("Sorry, user or password incorrect")
-        return
-    print("How do you want to proceed?")
-    option = int(input("1. Deposite o 2. Withdraw "))
-    ok, new_balance = action(option)
-    if ok:
-        print("Action successfully done...")
-        print("New balance:", new_balance)
-    else:
-        print("Action not permitted, saldo:")
-        print("New balance:", new_balance)
-
-
-execute()
