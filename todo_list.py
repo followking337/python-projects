98142101bdf8e0623dc481820393a66e40fb3b24
Daniel Folomkin Boldyrev <daniel773@icloud.com>
2022-02-28 16:59:02 +0100

    Improving code for project todo_list

--- todo_list.py
+++ todo_list.py
@@ -1,14 +1,3 @@
-# Menu Actions
-add = 1
-edit = 2
-done = 3
-delete = 4
-listTask = 5
-close = 6
-minimumOption = 1
-maximumOption = 6
-
-
 class Task:
 
     def __init__(self, name, due_date):
@@ -29,14 +18,8 @@ def menu():
     print('2. Edit Task')
     print('3. Mark Done')
     print('4. Delete Task')
-    print('5. View Tasks')
+    print('5. List Tasks')
     print('6. Quit')
-
-
-def title_list():
-    print('\n****************************')
-    print(' ****** TASKS LIST ******')
-    print('****************************')
 
 
 def get_option():
@@ -45,14 +28,17 @@ def get_option():
         print(f'Option {option} selected...')
     except ValueError:
         option = 0
-        print('This is not number, please, digit integer numbers from 1 till 6...')
+        print('This is not number, please, digit integer numbers from 1 to 6...')
     return option
 
 
-def create_task():
-    name = input('Name: ')
-    due_date = input('Date: ')
-    return Task(name, due_date)
+def get_index():
+    try:
+        index = int(input('Select the number of task:'))
+    except ValueError:
+        index = 0
+        print('This is not number, please, digit integer numbers from 1 to 6...')
+    return index
 
 
 def isValidOption(option):
@@ -61,108 +47,84 @@ def isValidOption(option):
     return True
 
 
-def new_list():
-    return []
+def create_task():
+    name = input('Name: ')
+    due_date = input('Date: ')
+    return Task(name, due_date)
 
 
-def add_task(task, toDo):
-    toDo.append(task)
-
-
-def process_add_task(toDo):
+def add_task(toDo):
     print('Creating Task...\n')
     task = create_task()
-    add_task(task, toDo)
+    toDo.append(task)
     print('\n---Task Created---')
 
 
-def all_tasks(toDo):
+def edit_task(toDo):
+    print('Editing Task...\n')
+    index = get_index()
+    toDo[index-1] = create_task()
+    print('\n---Task Edited---')
+
+
+def done_task(toDo):
+    print('Marking Task...\n')
+    index = get_index()
+    value = input('Press 1 if it is done, if not press any button...')
+    if value == '1':
+        done = True
+    else:
+        done = False
+    toDo[index-1].done = done
+    print('\n---Task Marked Done---')
+
+
+def delete_task(toDo):
+    print('Deleting Task...\n')
+    index = get_index()
+    del toDo[index-1]
+    print('\n---Task Deleted---')
+
+
+def title_list():
+    print('\n****************************')
+    print(' ****** TASKS LIST ******')
+    print('****************************')
+
+
+def list_tasks(toDo):
     index = 0
     for task in toDo:
         index += 1
         task.print_data(index)
 
 
-def get_index():
-    try:
-        index = int(input('Select the number of task:'))
-    except ValueError:
-        index = 0
-        print('This is not number, please, digit integer numbers from 1 till 6...')
-    return index
-
-
-def edit_task(index, new_task, toDo):
-    toDo[index-1] = new_task
-
-
-def setIsDone():
-    value = input('Press 1 if it is done, if not press any button...')
-    if value == '1':
-        return True
-    return False
-
-
-def doneTask(index, done, toDo):
-    toDo[index-1].done = done
-
-
-def processDoneTask(toDo):
-    print('Marking Task...\n')
-    index = get_index()
-    done = setIsDone()
-    doneTask(index, done, toDo)
-    print('\n---Task Marked---')
-
-
-def process_edit_task(toDo):
-    print('Editing Task...\n')
-    index = get_index()
-    task = create_task()
-    edit_task(index, task, toDo)
-    print('\n---Task Edited---')
-
-
-def deleteTask(index, toDo):
-    del toDo[index-1]
-
-
-def processDeleteTask(toDo):
-    print('Deleting Task...\n')
-    index = get_index()
-    deleteTask(index, toDo)
-    print('\n---Task Deleted---')
-
-
 def process(option, toDo):
-    print('The process is started...')
     message = '\nFunc is not implemented yet.\nSorry for inconvenience.'
     if option == 1:
-        process_add_task(toDo)
+        add_task(toDo)
     elif option == 2:
-        process_edit_task(toDo)
+        edit_task(toDo)
     elif option == 3:
-        processDoneTask(toDo)
+        done_task(toDo)
     elif option == 4:
-        processDeleteTask(toDo)
+        delete_task(toDo)
     elif option == 5:
         title_list()
-        all_tasks(toDo)
+        list_tasks(toDo)
 
 
 def execute():
     option = None
-    toDo = new_list()
+    todo_list = []
 
     while option != 6:
         menu()
         option = get_option()
         if not isValidOption(option):
-            print('\nOption not valid, try from 1 till 6.')
+            print('\nOption not valid, try from 1 to 6.')
             continue
-        process(option, toDo)
+        process(option, todo_list)
 
 
 execute()
-# task = functions.create_task()
-# task.print_data(5)
